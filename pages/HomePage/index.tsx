4e62831634ff51a18734121fd42152184cdc18bb
Telmo J Moura <telmop1999@gmail.com>
2023-01-10 17:49:30 +0000

    API response done

--- pages/HomePage/index.tsx
+++ pages/HomePage/index.tsx
@@ -1,5 +1,6 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Header } from "../../src/components/Header";
+import { api } from "../api/api";
 
 import { Body, Container, ImageBox, Subtitle, Title } from "./styles";
 
@@ -8,6 +9,24 @@ interface HomePageProps {
 }
 
 export function HomePage() {
+  const [data, setData] = useState();
+
+  const getAllData = () => {
+    api
+      .get(
+        "/1/posts?apikey=br7rqAj1hIO2XdNR&apitoken=a13zjd512nszxose&include=bodies,tags,photos,albums,authors,labels,audios,documents,dossiers,collections&filter[isoLanguage]=pt&filter[metadata.url]=criancas-que-jogam-videojogos-revelam-um-melhor-desempenho-cognitivo"
+      )
+      .then((response) => {
+        setData(response.data);
+      });
+  };
+
+  console.log(data);
+
+  useEffect(() => {
+    getAllData();
+  }, []);
+
   return (
     <>
       <Header />
@@ -27,16 +46,21 @@ export function HomePage() {
         <ImageBox>IMG</ImageBox>
 
         <Body>
-          <p>A investigação envolveu duas mil crianças entre os nove e os dez anos.
-          Os participantes foram divididos em dois grupos: no primeiro, estavam
-          os que nunca jogaram videojogos e, no segundo, encontravam-se as
-          crianças que jogavam diariamente três ou mais horas.</p>
-          <p>A investigação envolveu duas mil crianças entre os nove e os dez anos.
-          Os participantes foram divididos em dois grupos: no primeiro, estavam
-          os que nunca jogaram videojogos e, no segundo, encontravam-se as
-          crianças que jogavam diariamente três ou mais horas.</p>
+          <p>
+            A investigação envolveu duas mil crianças entre os nove e os dez
+            anos. Os participantes foram divididos em dois grupos: no primeiro,
+            estavam os que nunca jogaram videojogos e, no segundo,
+            encontravam-se as crianças que jogavam diariamente três ou mais
+            horas.
+          </p>
+          <p>
+            A investigação envolveu duas mil crianças entre os nove e os dez
+            anos. Os participantes foram divididos em dois grupos: no primeiro,
+            estavam os que nunca jogaram videojogos e, no segundo,
+            encontravam-se as crianças que jogavam diariamente três ou mais
+            horas.
+          </p>
         </Body>
-        
       </Container>
     </>
   );
